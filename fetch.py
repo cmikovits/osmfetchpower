3fdacff8a945da09cb65c9eccea8024801720c39
Christian Mikovits <christian.mikovits@boku.ac.at>
2020-01-21 16:25:52 +0100

    upd

--- fetch.py
+++ fetch.py
@@ -8,15 +8,15 @@ from OSMPythonTools.nominatim import Nominatim
 from collections import OrderedDict
 import time
 
-
-def fetchFeatures(areaId, year, osmkey, osmtype):
+def fetchFeatures(areaId, osmkey, osmtype):
     overpass = Overpass()
 
     query = overpassQueryBuilder(area=areaId,
-                                 elementType='node',
+                                 elementType=['node', 'way', 'relation'],
                                  selector='"' + osmkey + '"="' + osmtype + '"',
                                  includeGeometry=True,
                                  out='center meta')
+    logging.debug("OSM query: %s", query)
     return overpass.query(query, timeout=60)
 
 def fetchFeatureVersion(id, version):
@@ -34,30 +34,36 @@ def writeGEO(data, path, dataname):
 @click.option('-loglevel', '-l', help='log level (INFO, DEBUG)', default='DEBUG', type=str)
 def main(area, loglevel):
     logging.basicConfig(format='%(asctime)s, %(message)s',
-                       datefmt='%Y-%m-%d/ %H:%M:%S')
+                       datefmt='%Y-%m-%d %H:%M:%S')
     logging.getLogger().setLevel(loglevel)
 
     nominatim = Nominatim()
     areaId = nominatim.query(area).areaId()
     
     logging.debug("area: %s, id: %s",area,areaId)
-    
-    year = 2020
 
-    osmtypes = {'power': 'generator',
-                'power': 'plant'}
+    selectors = {'power': 'plant'}
     
-    for osmkey, osmval in osmtypes.items():
-        data = fetchFeatures(areaId, year, osmkey, osmval)
-
-    for i in range(0, data.countElements()):
-        id = data.elements()[i].id()
-        print(data.elements()[i].id())
-        print(data.elements()[i].lat(), " ", data.elements()[i].lat())
-        print(data.elements()[i].tags())
-        if (data.elements()[i].version() > 1):
-            print('older version: ', nelement.id())
-            print(nelement.timestamp())
+    for osmkey, osmval in selectors.items(): 
+        logging.debug("fetching %s = %s",osmkey, osmval)
+        data = fetchFeatures(areaId, osmkey, osmval)
+        logging.debug("Number of Elements: %s", data.countElements())
+        for i in range(0, 1):# data.countElements()):
+            e = data.elements()[i]
+            print(dir(e))
+            print(e.tags())
+            print(e.nodes())
+            #id = data.elements()[i].id()
+            #print(data.elements()[i].id())
+            #print(data.elements()[i].lat(), " ", data.elements()[i].lat())
+            #print(data.elements()[i].tags())
+            #print(data.elements()[i].timestamp())
+            #if (data.elements()[i].version() > 1):
+            #    nelement = fetchFeatureVersion(id, 1)
+            #    print('older version: ', nelement.id())
+            #    print(nelement.tags())
+            #    print(nelement.lat(), " ", nelement.lat())
+            #    print(nelement.timestamp())
 
 if __name__ == "__main__":
     main()
