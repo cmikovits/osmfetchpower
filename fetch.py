745d9ead689bf9920ddeafc1b90f5e80cd3be417
cmikovits <christian.mikovits@boku.ac.at>
2020-01-22 12:56:18 +0100

    new ver

--- fetch.py
+++ fetch.py
@@ -7,6 +7,8 @@ from OSMPythonTools.api import Api
 from OSMPythonTools.nominatim import Nominatim
 from collections import OrderedDict
 import time
+import pandas as pd
+import geopandas as gpd
 
 def fetchFeatures(areaId, osmkey, osmtype):
     overpass = Overpass()
@@ -23,6 +25,17 @@ def fetchFeatureVersion(id, version):
     api = Api()
     return(api.query('node/' + str(id) + '/1'))
     
+def addFeaturetoGDF(df, e):
+    edict = {'id': e.id(), 'lat':e.lat(), 'lon':e.lon(), **e.tags()}
+    print(edict)
+    dfr = pd.DataFrame([edict])
+    print(dfr)
+    exit(0)
+    #df.append({'lat':[e.lat()],
+    #           'lon':[e.lon()],
+    #          }, ignore_index=True)
+    return(df)
+    
 def writeGEO(data, path, dataname):
     data.to_file(filename = os.path.join(path, 'geojson', dataname+'.geojson'), driver="GeoJSON")
     data.to_file(filename = os.path.join(path, 'shape', dataname+'.shp'), driver = 'ESRI Shapefile')
@@ -30,7 +43,7 @@ def writeGEO(data, path, dataname):
     return(0)
 
 @click.command()
-@click.option('-area', '-a', help='area input', default='Vienna', type=str)
+@click.option('-area', '-a', help='area input', default='Aschau', type=str)
 @click.option('-loglevel', '-l', help='log level (INFO, DEBUG)', default='DEBUG', type=str)
 def main(area, loglevel):
     logging.basicConfig(format='%(asctime)s, %(message)s',
@@ -41,29 +54,26 @@ def main(area, loglevel):
     areaId = nominatim.query(area).areaId()
     
     logging.debug("area: %s, id: %s",area,areaId)
-
-    selectors = {'power': 'plant'}
+    selectors = {'power':'plant',
+                 'power':'generator'
+                 }
+    
+    df = pd.DataFrame()
     
     for osmkey, osmval in selectors.items(): 
         logging.debug("fetching %s = %s",osmkey, osmval)
         data = fetchFeatures(areaId, osmkey, osmval)
         logging.debug("Number of Elements: %s", data.countElements())
-        for i in range(0, 1):# data.countElements()):
+        for i in range(0, data.countElements()):
             e = data.elements()[i]
+            # id, lat, lon, timestamp, tags
             print(dir(e))
-            print(e.tags())
-            print(e.nodes())
-            #id = data.elements()[i].id()
-            #print(data.elements()[i].id())
-            #print(data.elements()[i].lat(), " ", data.elements()[i].lat())
-            #print(data.elements()[i].tags())
-            #print(data.elements()[i].timestamp())
-            #if (data.elements()[i].version() > 1):
-            #    nelement = fetchFeatureVersion(id, 1)
-            #    print('older version: ', nelement.id())
-            #    print(nelement.tags())
-            #    print(nelement.lat(), " ", nelement.lat())
-            #    print(nelement.timestamp())
+            #addFeaturetoGDF(df, e)
+            
+            #if getfirstrev:
+            #    if (data.elements()[i].version() > 1):
+            #        ne = fetchFeatureVersion(e.id(), 1)
+    print(df)                
 
 if __name__ == "__main__":
     main()
