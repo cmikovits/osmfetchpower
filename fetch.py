722f4f8167664ec4de00d775e1d921d3f66e8ae8
cmikovits <christian.mikovits@boku.ac.at>
2020-01-22 16:39:31 +0100

    working even with some quirks of osm

--- fetch.py
+++ fetch.py
@@ -6,7 +6,7 @@ from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
 from OSMPythonTools.api import Api
 from OSMPythonTools.nominatim import Nominatim
 from collections import OrderedDict
-import time
+import itertools
 import pandas as pd
 import geopandas as gpd
 
@@ -26,11 +26,46 @@ def fetchFeatureVersion(id, version):
     return(api.query('node/' + str(id) + '/1'))
     
 def addFeaturetoGDF(df, e):
-    edict = {'id': e.id(), 'lat':e.lat(), 'lon':e.lon(), **e.tags()}
-    print(edict)
+    lat = e.lat()
+    lon = e.lon()
+    if (e.type()=='way' or e.type() == 'relation'):
+        if (e.type()=='relation'):
+            polys = e.geometry()['coordinates']
+            points = []
+            for p in polys:
+                p = p[0]
+                points = points + p
+        else:
+            points = e.geometry()['coordinates'][0]
+        lat = []
+        lon = []
+        
+        # loop if there is more than one point (lat/lon) tuple
+        depth = lambda L: isinstance(L, list) and max(map(depth, L))+1
+                                                      
+        logging.debug("cols: %s, rows: %s", len(points), depth(points))
+        
+        if (depth(points) > 1):
+            for p in points:
+                lat.append(p[0])
+                lon.append(p[1])
+            lat = sum(lat)/len(lat)
+            lon = sum(lon)/len(lon)
+        else:
+            lat = points[0]
+            lon = points[1]
+    if (e.type()=='relation'):
+        polys = e.geometry()['coordinates']
+        points = []
+        for p in polys:
+            p = p[0]
+            points = points + p
+        
+        
+    edict = {'id': e.id(), 'lat':lat, 'lon':lon, 'timestamp':e.timestamp(), **e.tags()}
     dfr = pd.DataFrame([edict])
-    print(dfr)
-    exit(0)
+    #print(dfr)
+    df = pd.concat([df,dfr], axis=0, ignore_index=True, sort=False)
     #df.append({'lat':[e.lat()],
     #           'lon':[e.lon()],
     #          }, ignore_index=True)
@@ -43,7 +78,7 @@ def writeGEO(data, path, dataname):
     return(0)
 
 @click.command()
-@click.option('-area', '-a', help='area input', default='Aschau', type=str)
+@click.option('-area', '-a', help='area input', default='Vienna', type=str)
 @click.option('-loglevel', '-l', help='log level (INFO, DEBUG)', default='DEBUG', type=str)
 def main(area, loglevel):
     logging.basicConfig(format='%(asctime)s, %(message)s',
@@ -54,26 +89,32 @@ def main(area, loglevel):
     areaId = nominatim.query(area).areaId()
     
     logging.debug("area: %s, id: %s",area,areaId)
-    selectors = {'power':'plant',
-                 'power':'generator'
+    selectors = {'power':
+                    ['plant','generator']
                  }
     
     df = pd.DataFrame()
     
-    for osmkey, osmval in selectors.items(): 
-        logging.debug("fetching %s = %s",osmkey, osmval)
-        data = fetchFeatures(areaId, osmkey, osmval)
-        logging.debug("Number of Elements: %s", data.countElements())
-        for i in range(0, data.countElements()):
-            e = data.elements()[i]
-            # id, lat, lon, timestamp, tags
-            print(dir(e))
-            #addFeaturetoGDF(df, e)
-            
-            #if getfirstrev:
-            #    if (data.elements()[i].version() > 1):
-            #        ne = fetchFeatureVersion(e.id(), 1)
-    print(df)                
+    for osmkey, osmvalues in selectors.items():
+        for osmval in osmvalues:
+            logging.debug("fetching %s = %s",osmkey, osmval)
+            data = fetchFeatures(areaId, osmkey, osmval)
+            logging.debug("Number of Elements: %s", data.countElements())
+            for i in range(0, data.countElements()):
+                e = data.elements()[i]
+                if (e.tag('type')=='site'):
+                    print('site')
+                    continue
+                
+                # id, lat, lon, timestamp, tags
+                
+                df = addFeaturetoGDF(df, e)
+                
+                #if getfirstrev:
+                #    if (data.elements()[i].version() > 1):
+                #        ne = fetchFeatureVersion(e.id(), 1)
+    print(df)
+    df.to_csv(r'test.csv')                
 
 if __name__ == "__main__":
     main()
